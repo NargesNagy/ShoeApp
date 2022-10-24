273ec43a5616059667f29a2126d674599b06ca11
Narges Nagy <nargesnagy21@gmail.com>
2022-10-24 18:24:14 +0200

    final updat

--- app/src/main/java/com/example/shoeapp/bindingadapter/BindingAdapter.kt
+++ app/src/main/java/com/example/shoeapp/bindingadapter/BindingAdapter.kt
@@ -1,25 +1,6 @@
 package com.example.shoeapp.bindingadapter
 
-import androidx.databinding.BindingAdapter
-import androidx.databinding.InverseBindingAdapter
-import androidx.databinding.InverseMethod
-import com.google.android.material.textfield.TextInputLayout
 
 class BindingAdapter {
-/*
-    @InverseBindingAdapter("stringToInt")
-    fun intToString(value: Int?): String {
-        return value.toString() ?: ""
-    }
 
-
-    fun stringToInt(value: String): Int? {
-        return if (value.isNotEmpty()) {
-            value.toInt()
-        } else {
-            0
-        }
-    }
-
- */
 }
