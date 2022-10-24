83b30f65d5d4bef74f9d8f1abe8b6f4d8e82e8e9
Narges Nagy <nargesnagy21@gmail.com>
2022-10-24 18:34:48 +0200

    final upda

--- app/src/main/java/com/example/shoeapp/models/ShoeModel.kt
+++ app/src/main/java/com/example/shoeapp/models/ShoeModel.kt
@@ -10,14 +10,24 @@ import com.example.shoeapp.BR
  class ShoeModel : Parcelable , BaseObservable(){
     @get:Bindable
     var name: String? = null
-
+       set(value) {
+          field = value
+          notifyPropertyChanged(BR.name)
+       }
 
     @get:Bindable
     var company: String? = null
-
+       set(value) {
+          field = value
+          notifyPropertyChanged(BR.company)
+       }
 
     @get:Bindable
     var description: String? = null
+       set(value) {
+          field = value
+          notifyPropertyChanged(BR.description)
+       }
 
 
     @get:Bindable
